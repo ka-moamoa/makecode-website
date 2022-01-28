cb1fbb169d80fdcb4ba487e6a28b9b6da7a8e097
amyeguo <72362221+amyeguo@users.noreply.github.com>
2022-01-27 23:39:43 -0600

    Navbar, routing, and temp setup

---
+++ src/pages/Code.js
@@ -0,0 +1,12 @@
+//Import MaterialUI Components
+import Typography from "@mui/material/Typography";
+
+const Code = () => {
+  return (
+    <Typography variant="h2" align="center" sx={{ mt: 5 }}>
+      Code & Design
+    </Typography>
+  );
+};
+
+export default Code;
---
+++ src/pages/Homepage.js
@@ -0,0 +1,39 @@
+//Import MaterialUI Components
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+import CardMedia from "@mui/material/CardMedia";
+
+const Homepage = () => {
+  return (
+    <>
+      <Typography variant="h2" align="center" sx={{ mt: 5 }}>
+        MakeCode Iceberg
+      </Typography>
+      <Typography variant="h4" align="center">
+        Battery-free MakeCode
+      </Typography>
+      <Grid container justifyContent="center" sx={{ mt: 5 }}>
+        <Card sx={{ maxWidth: 400 }}>
+          <CardMedia
+            component="iframe"
+            alt="MakeCode Iceberg Overview Video"
+            src="https://www.youtube.com/embed/aaBHzfc0KOg"
+            sx={{ height: 300, width: 500 }}
+          />
+        </Card>
+      </Grid>
+      <Typography variant="body1" sx={{ margin: 5 }} align="center">
+        Microsoft MakeCode is one of the most popular platforms for learned and
+        teaching computer science. It is found in classrooms, as well as used by
+        the “maker” movement. Our project, Battery-free MakeCode transforms
+        standard Microsoft MakeCode programs to add additional system support,
+        which allows these programs to run on harvested energy. It enables
+        novice programmers to take their in-class/in-lab fun projects to the
+        wild/in the real world without having to power them through batteries.
+      </Typography>
+    </>
+  );
+};
+
+export default Homepage;
